cc7e202c5e5db22d6f4bd349fa64b3f396da8263
Urchmaney <kingsobino@gmail.com>
2019-08-26 22:58:08 +0000

    Work on the forms for different rails tags

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-			redirect_to new_user_path
+			redirect_to new_users_path
 		else
 			render :new
 		end
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:user][:id])
+    @user = User.find(params[:id].to_i)
 
-		if @user.save
+		if @user.update(user_params)
 			redirect_to new_user_path
 		else
 			render :edit
