5c1280ada091c7d2ece688b2787bc210add97ee4
SamuelC28 <samuel@noukod.com>
2019-08-26 17:26:49 -0400

    controller modified and route added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,12 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:user][:id])
-
-		if @user.save
-			redirect_to new_user_path
-		else
-			render :edit
+    @user = User.find(params[:id])
+    if 
+      @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
     end
   end
     
