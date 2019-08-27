6e021231efe6acea792fd3900857db833c3c6143
Urchmaney <kingsobino@gmail.com>
2019-08-27 16:36:46 +0000

    changes from review

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@
 class User < ApplicationRecord
   validates :username, presence: true
   validates :email, presence: true
+  validates :email, :format => /@/
   validates :password, presence: true
 end
